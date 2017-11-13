f23c4431a3830d4a2bed4dcb176051c990a312ea
Shunsuke Hiratsuka <hiratsukashu@indival.co.jp>
2017-11-13 15:17:39 +0900

    test

---
+++ image_fetcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python3 image_fetcher.py '桃田賢斗' data_momota
+python3 image_fetcher.py '奥原希望' data_okuhara
+python3 image_fetcher.py '山口茜' data_yamaguchi
+python3 image_fetcher.py '林丹' data_rindan
+python3 image_fetcher.py 'タイ・ツーイン' data_tai
